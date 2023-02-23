1223dd1680f26b738d56f133bc20c7b94947def4
pateld23atwit <55283003+pateld23atwit@users.noreply.github.com>
2023-02-23 11:33:22 -0500

    Added initial program code.

--- app/src/main/java/com/example/rgbcontrollerui/Main.java
+++ app/src/main/java/com/example/rgbcontrollerui/Main.java
@@ -1,4 +1,92 @@
 package com.example.rgbcontrollerui;
 
-public class Main {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
+
+public class Main extends AppCompatActivity {
+    private Toolbar toolbar;
+    private Button mSetColorButton, mPickColorButton;
+    private View mColorPreview;
+    private TextView textView;
+    private int mDefaultColor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setToolbar();
+        mPickColorButton = findViewById(R.id.pick_color_button);
+        mSetColorButton = findViewById(R.id.set_color_button);
+        mColorPreview = findViewById(R.id.preview_selected_color);
+        textView = findViewById(R.id.textView);
+        mDefaultColor = 0;
+
+        mPickColorButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // to make code look cleaner the color
+                        // picker dialog functionality are
+                        // handled in openColorPickerDialogue()
+                        // function
+                        openColorPickerDialogue();
+                    }
+                });
+
+        // button to set the color GFG text
+        mSetColorButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // as the mDefaultColor is the global
+                        // variable its value will be changed as
+                        // soon as ok button is clicked from the
+                        // color picker dialog.
+                        textView.setTextColor(mDefaultColor);
+                    }
+                });
+    }
+    private void setToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        TextView title = toolbar.findViewById(R.id.toolbarTitle);
+        title.setText("ESP32 RGB Controller");
+    }
+    public void openColorPickerDialogue() {
+
+        // the AmbilWarnaDialog callback needs 3 parameters
+        // one is the context, second is default color,
+        final AmbilWarnaDialog colorPickerDialogue = new AmbilWarnaDialog(this, mDefaultColor,
+                new AmbilWarnaDialog.OnAmbilWarnaListener() {
+                    @Override
+                    public void onCancel(AmbilWarnaDialog dialog) {
+                        // leave this function body as
+                        // blank, as the dialog
+                        // automatically closes when
+                        // clicked on cancel button
+                    }
+
+                    @Override
+                    public void onOk(AmbilWarnaDialog dialog, int color) {
+                        // change the mDefaultColor to
+                        // change the GFG text color as
+                        // it is returned when the OK
+                        // button is clicked from the
+                        // color picker dialog
+                        mDefaultColor = color;
+
+                        // now change the picked color
+                        // preview box to mDefaultColor
+                        mColorPreview.setBackgroundColor(mDefaultColor);
+                    }
+                });
+        colorPickerDialogue.show();
+    }
 }
