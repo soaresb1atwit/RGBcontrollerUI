463b6ca2ea35190657e8f4023cece8aa8e91d348
stenw <stenw@wit.edu>
2023-04-06 10:03:55 -0400

    Updated socket manager with running tcp connetion. Hi gang.

--- app/src/main/java/com/example/rgbcontrollerui/network/SocketManager.java
+++ app/src/main/java/com/example/rgbcontrollerui/network/SocketManager.java
@@ -1,7 +1,10 @@
 package com.example.rgbcontrollerui.network;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,9 +12,45 @@ import java.util.List;
 public class SocketManager
 {
     private final List<Socket> sockets;
+    private final int port = 1234;
+    private final Socket socket ;
 
-    public SocketManager()
+   /* public void serverSocket(int port) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while(true) {
+                InputStream input = serverSocket.accept().getInputStream();
+                InputStreamReader reader = new InputStreamReader(input);
+                int digit = reader.read();  // reads a single character
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }*/
+ void run() {
+     try {
+         int serverPort = 1111 ;
+         InetAddress host = InetAddress.getByName("localhost") ;
+         System.out.println("Connecting to server on port " + serverPort) ;
+         System.out.println("Just connected to " + this.socket.getRemoteSocketAddress());
+         PrintWriter toServer =
+                 new PrintWriter(socket.getOutputStream(),true);
+         BufferedReader fromServer =
+                 new BufferedReader(
+                         new InputStreamReader(socket.getInputStream()));
+         toServer.println("Hello from " + socket.getLocalSocketAddress());
+         String line = fromServer.readLine();
+         System.out.println("Client received: " + line + " from Server");
+     } catch (IOException e) {
+         throw new RuntimeException(e);
+     }
+ }
+
+
+    public SocketManager(Socket socket)
     {
+        this.socket = socket;
         this.sockets = new ArrayList<>();
     }
 
